9b5c8b052e625135eb65464cc9cf889689b41260
Emi ONeal <atashiemichandesu@gmail.com>
2023-05-26 22:28:17 -0400

    added firebase

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: `${FIREBASE}`,
+  authDomain: "atlantean-depth-345204.firebaseapp.com",
+  projectId: "atlantean-depth-345204",
+  storageBucket: "atlantean-depth-345204.appspot.com",
+  messagingSenderId: "376917534798",
+  appId: "1:376917534798:web:06b6e27cced1c407ed7b1f",
+  measurementId: "G-PHSYYXRQE5"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const express = require('express')
 const app = express()
 require('dotenv').config()
