7fa56cf410e3cdaf0a1fa9e52cd6b1f7b79aa8eb
Emi ONeal <atashiemichandesu@gmail.com>
2023-06-12 16:47:40 -0400

    edited server file to allow deployment

--- server.js
+++ server.js
@@ -1,31 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: `${FIREBASE}`,
-  authDomain: "atlantean-depth-345204.firebaseapp.com",
-  projectId: "atlantean-depth-345204",
-  storageBucket: "atlantean-depth-345204.appspot.com",
-  messagingSenderId: "376917534798",
-  appId: "1:376917534798:web:06b6e27cced1c407ed7b1f",
-  measurementId: "G-PHSYYXRQE5"
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 const express = require('express')
 const app = express()
 require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const { expressjwt } = require('express-jwt')
+
+// ... other imports
+const path = require("path")
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -34,6 +15,9 @@ mongoose.connect(
   `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.k5floph.mongodb.net/`,
   () => console.log('Connected to the DB')
 )
+
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use('/auth', require('./routes/authRouter.js'))
 app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256']})) // req.user
@@ -46,6 +30,10 @@ app.use((err, req, res, next) => {
   }
   return res.send({errMsg: err.message})
 })
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(9000, () => {
   console.log(`Server is running on local port 9000`)
